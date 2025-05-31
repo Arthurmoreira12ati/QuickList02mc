cea288e5324bc536e91635306a265c3a0cde970d
Arthur moreira <arthurmsouza416@gmail.com>
2025-05-30 23:42:53 -0300

    adição do array de items no localStorage e criação da função verifyLocalSotrageItems para verificar se já existem iens sempre que o projeo for renderizado

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const items = []
+let items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
 
-    if (itemName === " ") {
+    if (itemName === "") {
         alert("Digite um item válido")
         return
     }
@@ -41,6 +41,8 @@ function showItemsList() {
                 </div>
         `
     })
+
+    localStorage.setItem("items", JSON.stringify(items))
 }
 
 function checkItem(itemName) {
@@ -70,3 +72,14 @@ function removeItem(itemName) {
 function addHideWarningClass() {
     document.querySelector(".warning").classList.add("hide-warning")
 }
+
+function verifyLocalStorageItems(){
+    const localStorageItems = localStorage.getItem("items")
+
+    if (localStorageItems) {
+        items = JSON.parse(localStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems()
